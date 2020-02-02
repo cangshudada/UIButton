24df08ee478a4a57b218c5bf56e274e88ed33fc3
cangshudada <15342240569@163.com>
2020-02-02 17:07:42 +0800

    练习项目

---
+++ src/Home.vue
@@ -0,0 +1,118 @@
+<template>
+  <div id="home">
+    <div class="btn-container">
+      <UIButton
+        @click="onClick"
+        :mini="mini"
+        :small="small"
+        :medium="medium"
+        :large="large"
+        :xlarge="xlarge"
+        :tile="tile"
+        :rounded="rounded"
+        :circle="circle"
+      >Hello</UIButton>
+    </div>
+    <div class="btn-group">
+      <UIButton class="btn" @click="resize('mini')">超小</UIButton>
+      <UIButton class="btn" @click="resize('small')">小</UIButton>
+      <UIButton class="btn" @click="resize('medium')">正常</UIButton>
+      <UIButton class="btn" @click="resize('large')">大</UIButton>
+      <UIButton class="btn" @click="resize('xlarge')">超大</UIButton>
+    </div>
+
+    <div class="btn-group">
+      <UIButton class="btn" @click="changeRedius('tile')">方形</UIButton>
+      <UIButton class="btn" @click="changeRedius('normal')">正常</UIButton>
+      <UIButton class="btn" @click="changeRedius('rounded')">半圆形</UIButton>
+      <UIButton class="btn" @click="changeRedius('circle')">圆形</UIButton>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from "vue-property-decorator";
+import UIButton from "@/components/UIkit/UIbutton.vue";
+
+@Component({
+  components: {
+    UIButton
+  }
+})
+export default class Home extends Vue {
+  private mini: boolean = false;
+  private small: boolean = false;
+  private medium: boolean = true;
+  private large: boolean = false;
+  private xlarge: boolean = false;
+
+  private tile: boolean = false;
+  private rounded: boolean = false;
+  private normal: boolean = true;
+  private circle: boolean = false;
+
+  private onClick(event: MouseEvent) {
+    console.log("hello");
+  }
+
+  private resize(name: string) {
+    this.mini = false;
+    this.small = false;
+    this.medium = false;
+    this.large = false;
+    this.xlarge = false;
+    switch (name) {
+      case "mini":
+        this.mini = true;
+        break;
+      case "small":
+        this.small = true;
+        break;
+      case "medium":
+        this.medium = true;
+        break;
+      case "large":
+        this.large = true;
+        break;
+      case "xlarge":
+        this.xlarge = true;
+        break;
+    }
+  }
+
+  private changeRedius(radius: string) {
+    this.tile = false;
+    this.rounded = false;
+    this.normal = false;
+    this.circle = false;
+    switch (radius) {
+      case "tile":
+        this.tile = true;
+        break;
+      case "rounded":
+        this.rounded = true;
+        break;
+      case "normal":
+        this.normal = true;
+        break;
+      case "circle":
+        this.circle = true;
+        break;
+    }
+  }
+}
+</script>
+
+<style scoped lang='stylus'>
+.btn-container {
+  width: 480px;
+  height: 160px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.btn {
+  margin: 6px;
+}
+</style>
---
+++ src/components/UIkit/UIbutton.vue
@@ -0,0 +1,105 @@
+<template>
+  <button
+    class="ui-btn"
+    :class="{
+    // size
+    'ui-btn-mini': mini,
+    'ui-btn-small': small,
+    'ui-btn-large': large,
+    'ui-btn-xlarge': xlarge,
+
+    // radius
+    'ui-btn-tile': tile,
+    'ui-btn-rounded': rounded,
+    'ui-btn-circle': circle,
+
+    // disabled
+    'ui-btn-disabled': disabled,
+  }"
+    @click="onClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang='ts'>
+import { Vue, Component, Emit, Prop } from "vue-property-decorator";
+
+@Component
+export default class UIbutton extends Vue {
+  // size
+  @Prop(Boolean) private mini: boolean | undefined;
+  @Prop(Boolean) private small: boolean | undefined;
+  @Prop(Boolean) private large: boolean | undefined;
+  @Prop(Boolean) private xlarge: boolean | undefined;
+
+  // radius
+  @Prop(Boolean) private tile: boolean | undefined;
+  @Prop(Boolean) private rounded: boolean | undefined;
+  @Prop(Boolean) private circle: boolean | undefined;
+
+  // disabled
+  @Prop(Boolean) private disabled: boolean | undefined;
+
+  // 传递点击事件
+  @Emit("click") private emitClickEvent(event: MouseEvent) {}
+
+  private mounted() {}
+
+  private onClick(event: MouseEvent) {
+    this.emitClickEvent(event);
+  }
+}
+</script>
+
+<style scoped lang='stylus'>
+resize (minWidth, height, padding, fontSize) {
+  min-width: minWidth;
+  height: height;
+  padding: 0 padding;
+  font-size: fontSize;
+
+  // &.ui-btn-rounded, &.ui-btn-circle {
+  //   border-radius: (@height / 2);
+  // }
+
+  // &.ui-btn-circle {
+  //   padding: 0;
+  //   min-width: 0;
+  //   width: @height;
+  // }
+}
+
+.ui-btn {
+  border: 0 solid black;
+  color: #17233d;
+  background-color: #2d8cf0;
+  border-radius: 4px;
+  outline: none;
+  font-weight: 500;
+  letter-spacing: 0.09em;
+  cursor: pointer;
+  user-select: none;
+  resize(64px, 36px, 16px, 0.875rem);
+  &.ui-btn-mini {
+    resize(36px, 20px, 9px, 0.625rem);
+  }
+
+  &.ui-btn-small {
+    resize(50px, 28px, 12px, 0.75rem);
+  }
+
+  &.ui-btn-large {
+    resize(78px, 44px, 19px, 0.75rem);
+  }
+
+  &.ui-btn-xlarge {
+    resize(92px, 52px, 23px, 1rem);
+  }
+
+  &.ui-btn-tile {
+    border-radius: 0px;
+  }
+}
+</style>
+
